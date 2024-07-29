6e0ad8fa3f24c9e473c0e7ad61ba63ac6bf8e163
KurshedKhan <kurshedkhanbhadra@gmail.com>
2024-07-29 15:48:27 +0530

    Array

---
+++ JavaScript/Array1.js
@@ -0,0 +1,18 @@
+let student = ["Bhavesh",101,"BCA"];
+
+console.log(`Name : ${student[0]} and roll is ${student[1]}.`);
+
+let cars = ["Alto","Alto800","AltoK10","AltoFullyModified"];
+
+console.log(cars[2]);
+
+let Data = [];
+
+Data[0] = "Bhavesh";
+Data[1] = "Vinay";
+Data[2] = "vikash";
+
+console.log("First Name : ",Data[0]);
+
+let Section = student[2];
+console.log(Section)
